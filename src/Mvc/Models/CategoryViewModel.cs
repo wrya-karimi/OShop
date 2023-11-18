463da340e6bdba46e50a36d4ad116a62140c77e0
USER <USER@DESKTOP-IPUL0QJ>
2023-11-18 15:00:19 +0330

    Remove Extra project

--- src/Mvc/Models/CategoryViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Mvc.Models
-{
-    public class CategoryViewModel
-    {
-        public int Id { get; set; }
-        public required string Name { get; set; }
-        public string? Description { get; set; }
-    }
-}
--- src/Mvc/Models/Item.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mvc.Models
-{
-    public class Item
-    {
-        public Product Product { get; set; }
-        public int Quantity { get; set; }
-    }
-}
